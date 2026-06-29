1fa32a16578553816383079896453a711298ae1e
Jakub Stachurski <jakub@wilkuu.xyz>
2026-06-29 18:21:41 +0200

    test(cli): skip emit_upsert_writes_match_on_from_label_property test

--- packages/stalwart-cli/package.nix
+++ packages/stalwart-cli/package.nix
@@ -40,6 +40,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "commands::snapshot::tests::emit_create_flushes_sink_on_empty_shard"
     "commands::snapshot::tests::emit_create_omits_deferred_field_and_emits_followup_update"
     "commands::snapshot::tests::snapshot_emits_marker_only_variant_as_type_only_record"
+    "commands::snapshot::tests::emit_upsert_writes_match_on_from_label_property"
   ] (test: "--skip=${test}");
 
   doInstallCheck = true;
